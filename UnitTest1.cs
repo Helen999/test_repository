8562c7a0af1ec5239ba54e4baad2805aafc4535e
Helen999 <levina.av@gmail.com>
2017-09-01 15:57:47 +0600

    2-nd lesson

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
@@ -16,15 +16,16 @@ namespace lesson_first
         {
             driver = new OpenQA.Selenium.Chrome.ChromeDriver();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            
+
         }
         [TestMethod]
         public void TestMethod1()
         {
             driver = new ChromeDriver();
-            driver.Url = "http://google.com";
-            driver.FindElement(OpenQA.Selenium.By.Name("q")).SendKeys("webdriver");
-            driver.FindElement(OpenQA.Selenium.By.Name("btng")).Click();
+            driver.Url = "http://localhost/litecart/admin/login.php";
+            driver.FindElement(OpenQA.Selenium.By.Name("username")).SendKeys("admin");
+            driver.FindElement(OpenQA.Selenium.By.Name("password")).SendKeys("admin");
+            driver.FindElement(OpenQA.Selenium.By.Name("login")).Click();
             wait.Until(ExpectedConditions.TitleIs("webdriver - поиск в Google"));
         }
 
